2ad539ca127f7853401c794e1d450d016f21c6c1
Jonathan Wang <wangjonathan1992@gmail.com>
2018-11-01 10:15:35 -0400

    fix issue: https://stackoverflow.com/questions/35796256/express-webpack-failing-on-dokku-and-heroku/37740210\#37740210

--- server/server.js
+++ server/server.js
@@ -4,9 +4,18 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.use('/public', express.static(publicPath));
+app.use(express.static(publicPath));
+
+app.get(path.join(publicPath, 'bundle.js'), (req, res) => {
+  res.sendFile(path.join(publicPath, 'bundle.js'));
+});
+
+app.get(path.join(publicPath, 'vendor.js'), (req, res) => {
+  res.sendFile(path.join(publicPath, 'vendor.js'));
+});
 
 app.get('*', (req, res) => {
+  console.log(publicPath);
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
